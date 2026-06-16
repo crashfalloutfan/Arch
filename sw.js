8f03f01e22d249295e2c066c50147718d6868d57
crashfalloutfan <aaronjacob104@gmail.com>
2026-06-15 21:18:20 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.3/dist/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(sw.fetch(event));
-});
